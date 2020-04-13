1995c8e0bf52ad5b65b07c4b7e4cfda80a48f427
AislynnEdmiston <davidandaislynn@gmail.com>
2020-04-13 00:41:22 -0400

    set up navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Contact Us</a>
+                <a href="#">About</a>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
